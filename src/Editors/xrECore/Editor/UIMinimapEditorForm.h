c699005f4fe65c7fb6b19d5160617889f1ed5761
GladiatorVS <gladiatorvsoff@gmail.com>
2024-11-14 16:39:28 +0300

    Global Map Editor Test

--- src/Editors/xrECore/Editor/UIMinimapEditorForm.h
+++ src/Editors/xrECore/Editor/UIMinimapEditorForm.h
@@ -4,6 +4,23 @@
 class UIMinimapEditorForm:
 	public IEditorWnd
 {
+private:
+	struct Element {
+		xr_string name;
+		xr_string path;
+		ImVec2 position;
+		ImVec2 RenderSize;
+		ImVec2 FileSize;
+		ImTextureID	texture;
+		bool selected = false;
+	};
+
+	template<typename T>
+	T Clamp(T value, T min, T max) {
+		if (value < min) return min;
+		if (value > max) return max;
+		return value;
+	}
 public:
 	UIMinimapEditorForm();
 	virtual ~UIMinimapEditorForm();
@@ -17,16 +34,45 @@ private:
 	static UIMinimapEditorForm* Form;
 
 private:
-	ImTextureID					m_Texture; 
-	ImTextureID					m_TextureRemove;
+	void LoadBGClick(const xr_string = "");
+	int LoadTexture(Element&, const xr_string = "");
 
+	void ShowMenu();
+	void RenderCanvas();
+	void ShowElementList();
+	void ShowPreview();
+	void UnselectAllElements();
+	void SelectElement(Element&);
+
+	void SaveFile(bool);
+	void OpenFile();
+
+	xr_vector<Element> elements;
+
+	bool m_AlwaysDrawBorder = false;
+	bool isDragging = false;
+	bool PreviewMode = false;
+
+	float m_Zoom = 1.0f;
+	int m_ItemsOpacity = 255;
+
+	ImVec2 initial_bg_position;
+	ImVec2 initial_mouse_pos;
+	
+	Element* selectedElement=nullptr;
+
+	xr_string ActiveFile;
+	xr_string ActiveFileShort;
+	bool isEdited;
 private:
-	void LoadClick();
+	ImTextureID					m_BackgroundTexture;
+	ImTextureID					m_TextureRemove;
+	//
+	ImVec2 m_BackgroundPosition;
+	//
 	U32Vec      m_ImageData;
-	u32         m_ImageW;
-	u32         m_ImageH;
+	u32         m_ImageW = 128;
+	u32         m_ImageH = 128;
 	u32         m_ImageA;
-	Fbox2       map_bb;
-	Fbox2       map_bb_loaded;
-	Ivector2    image_draw_size;
+	xr_string	m_BackgroundTexturePath;
 };
