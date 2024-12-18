f02f298fb7c9329047ca4873343c7e9510102b94
Drombeys <dima.zolotov02@gmail.com>
2024-12-19 00:34:05 +0300

    закрытие

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -30,6 +30,7 @@ add_compile_options("$<$<CONFIG:RELEASE>:/GL>" "$<$<CONFIG:RELWITHDEBINFO>:/GL>"
 add_compile_options("$<$<CONFIG:RELWITHDEBINFO>:/Ob2>")
 add_compile_options("$<$<CONFIG:RELWITHDEBINFO>:/Ot>")
 add_link_options("$<$<CONFIG:RELEASE>:/LTCG:incremental>" "$<$<CONFIG:RELWITHDEBINFO>:/LTCG:incremental>")
+add_link_options("$<$<CONFIG:RELEASE>:/INCREMENTAL:NO>" "$<$<CONFIG:RELWITHDEBINFO>:/INCREMENTAL:NO>")
 
 ## Exceptions...
 add_compile_options("$<$<CONFIG:DEBUG>:/EHsc>")
