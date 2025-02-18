d5e4c93c8ec1acfdf332946fd4be163571e56504
mnelenpridumivat <ragrind@mail.ru>
2025-02-18 13:46:04 +0300

    Fix editor building

--- src/Editors/xrECore/Editor/render.cpp
+++ src/Editors/xrECore/Editor/render.cpp
@@ -469,7 +469,7 @@ void 	CRender::set_Transform(Fmatrix* M)
 	current_matrix.set(*M);
 }
 
-void CRender::add_Visual(IRenderVisual* visual, bool) {
+void CRender::add_Visual(IRenderVisual* visual, bool, bool Force) {
 	if(val_bInvisible) {
 		return;
 	}
