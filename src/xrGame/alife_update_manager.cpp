3983764de3652f7f9b39b8577fa4366ad3c98f60
mnelenpridumivat <ragrind@mail.ru>
2025-03-29 14:40:13 +0300

    Fix level changers

--- src/xrGame/alife_update_manager.cpp
+++ src/xrGame/alife_update_manager.cpp
@@ -185,7 +185,7 @@ bool CALifeUpdateManager::change_level	(NET_Packet &net_packet)
 	// then change actor server entity 
 	// then call client net_Save 
 	// then restore actor server entity 
-	Level().ClientSend				();
+	//Level().ClientSend				();
 
 	m_changing_level				= true;
 	
@@ -206,7 +206,7 @@ bool CALifeUpdateManager::change_level	(NET_Packet &net_packet)
 	net_packet.r_vec3				(graph().actor()->o_Position);
 	net_packet.r_vec3				(graph().actor()->o_Angle);
 
-	Level().ClientSave				();
+	//Level().ClientSave				();
 
 	graph().actor()->o_torso.yaw	= graph().actor()->o_Angle.y;
 	graph().actor()->o_torso.pitch	= graph().actor()->o_Angle.x;
