6a5f87d945b89a19233439b9b1c7a56c1457d144
wh1t3lord <git.matvey@yandex.ru>
2025-04-08 22:32:45 +0300

    Add hud adjust #4;

--- src/xrGame/ImUtils/HudAdjustManager.cpp
+++ src/xrGame/ImUtils/HudAdjustManager.cpp
@@ -11,6 +11,38 @@
 
 #include "ImUtils.h"
 
+void ImGui_Render2DWidget()
+{
+	static ImVec2 scrolling(0.0f, 0.0f);
+
+	// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+	ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
+	ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
+//	canvas_p0.x += canvas_sz.x * 0.5f;
+//	canvas_p0.y += canvas_sz.y * 0.5f;
+	canvas_sz.x = (canvas_sz.x < canvas_sz.y ? canvas_sz.x : canvas_sz.y) * 0.5f;
+	canvas_sz.y = canvas_sz.x;
+
+	ImVec2 reg = ImGui::GetContentRegionAvail();
+
+//	canvas_p0.x += reg.x - canvas_sz.x;
+//	canvas_p0.y += reg.y - canvas_sz.y;
+
+	ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+	// Draw border and background color
+	ImGuiIO& io = ImGui::GetIO();
+	ImDrawList* draw_list = ImGui::GetWindowDrawList();
+	draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+	draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+	// This will catch our interactions
+	ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+	const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+	const bool is_active = ImGui::IsItemActive();   // Held
+	const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+	const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+}
 
 
 void RenderHUDAdjustManager()
@@ -46,11 +78,25 @@ void RenderHUDAdjustManager()
 						// todo: implement
 					}
 
-					ImGui::SliderFloat("X##HUDP", &imgui_hud_adjust_manager.settings.hud_position.x, -1.0f, 1.0f);
+					if (ImGui::BeginTable("Data##HUDP", 2))
+					{
+						ImGui::TableNextRow();
 
-					ImGui::SliderFloat("Y##HUDP", &imgui_hud_adjust_manager.settings.hud_position.y, -1.0f, 1.0f);
+						ImGui::TableNextColumn();
 
-					ImGui::SliderFloat("Z##HUDP", &imgui_hud_adjust_manager.settings.hud_position.z, -1.0f, 1.0f);
+						ImGui::SliderFloat("X##HUDP", &imgui_hud_adjust_manager.settings.hud_position.x, -1.0f, 1.0f);
+
+						ImGui::SliderFloat("Y##HUDP", &imgui_hud_adjust_manager.settings.hud_position.y, -1.0f, 1.0f);
+
+						ImGui::SliderFloat("Z##HUDP", &imgui_hud_adjust_manager.settings.hud_position.z, -1.0f, 1.0f);
+						
+						auto test = ImGui::GetContentRegionAvail();
+						ImGui::TableNextColumn();
+						ImGui_Render2DWidget();
+
+						ImGui::EndTable();
+					}
+
 
 
 
