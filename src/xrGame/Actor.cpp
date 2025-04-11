d2d70424f8111ac2761d0e58f5d0028e454292b4
Shtrecker <gogels235@gmail.com>
2025-04-11 10:13:42 +0300

    Фикс холдеров и удаления худа (схлопнуть)

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1423,10 +1423,9 @@ void CActor::UpdatePlayerView()
 
 	setVisible(has_visible, has_shadow_only);
 
-	/*if (IsFocused())
+	if (IsFocused())
 	{
-		BOOL bHudView = HUDview();
-		if (bHudView)
+		if (!m_holder || m_holder->HUDView() && m_holder->allowWeapon())
 		{
 			CInventoryItem* pInvItem = inventory().ActiveItem();
 			if (pInvItem)
@@ -1466,7 +1465,7 @@ void CActor::UpdatePlayerView()
 		{
 			g_player_hud->detach_all_items();
 		}
-	}*/
+	}
 
 	float dt = Device.fTimeDelta;
 
