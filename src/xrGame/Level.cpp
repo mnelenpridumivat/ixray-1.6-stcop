2a9795e7b70f2b734369911f33f16a25ee1e5287
v2v3v4 <v2v3v4@users.noreply.github.com>
2025-05-29 12:42:15 +0300

    Add profiler markers

--- src/xrGame/Level.cpp
+++ src/xrGame/Level.cpp
@@ -85,6 +85,8 @@ u32			lvInterpSteps		= 0;
 CLevel::CLevel():
 	IPureClient(Device.GetTimerGlobal())
 {
+	PROF_EVENT("CLevel::CLevel");
+	
 	LoadCallbackGlobals(m_isStartAttack, m_onStartAttack, "OnStartAttack");
 	LoadCallbackGlobals(m_isKeyPress, m_onKeyPress, "OnKeyPress");
 
@@ -198,6 +200,7 @@ extern CAI_Space *g_ai_space;
 
 CLevel::~CLevel()
 {
+	PROF_EVENT("CLevel::~CLevel");
 	DestroyImGuiInGame();
 	xr_delete					(g_player_hud);
 	delete_data					(hud_zones_list);
