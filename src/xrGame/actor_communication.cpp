f8adc0bdb517da8213fd8ab920c029a1c2f70663
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-06-22 12:37:48 +0300

    Fix exit button in talk dialog for CS

--- src/xrGame/actor_communication.cpp
+++ src/xrGame/actor_communication.cpp
@@ -161,6 +161,9 @@ void   CActor::UpdateAvailableDialogs	(CPhraseDialogManager* partner)
 	for(u32 i = 0; i<pInvOwnerPartner->CharacterInfo().ActorDialogs().size(); i++)
 		AddAvailableDialog(pInvOwnerPartner->CharacterInfo().ActorDialogs()[i], partner);
 
+	if (EngineExternal().ClearSkyMode())
+		AddAvailableDialog("actor_break_dialog", partner);
+
 	CPhraseDialogManager::UpdateAvailableDialogs(partner);
 }
 
@@ -179,10 +182,12 @@ void CActor::RunTalkDialog(CInventoryOwner* talk_partner, bool disable_break)
 	{	
 		StartTalk(talk_partner);
 
-		if(CurrentGameUI()->TopInputReceiver())
+		if (CurrentGameUI()->TopInputReceiver())
+		{
 			CurrentGameUI()->TopInputReceiver()->HideDialog();
-
- 		CurrentGameUI()->StartTalk(talk_partner->bDisableBreakDialog);
+		}
+		bool disableBreakDialog = EngineExternal().ClearSkyMode() ? disable_break : talk_partner->bDisableBreakDialog;
+ 		CurrentGameUI()->StartTalk(disableBreakDialog);
 	}
 }
 
