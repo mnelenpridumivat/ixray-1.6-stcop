bc3d46d8b863f51c0e3ee3dbff659b1c182d72ad
ForserX <sv3nk@yandex.ru>
2025-06-28 15:37:19 +0300

    Fix warnings in `Particles Editor` linker

--- src/Editors/ParticleEditor/ParticleEffectActionsImpl.cpp
+++ src/Editors/ParticleEditor/ParticleEffectActionsImpl.cpp
@@ -12,7 +12,7 @@ using namespace PAPI;
 #define PARTICLE_ACTION_VERSION_MIN 0x0000
 #define PARTICLE_ACTION_VERSION_MAX 0x0001
 //---------------------------------------------------------------------------
-xr_token2					actions_token		[ ]={
+xr_token2 actions_token_impl [] = {
 	{ "Avoid",				"Steer particles away from a domain of space.", 			                PAAvoidID				},        
 	{ "Bounce",				"Bounce particles off a domain of space.",					                PABounceID				},        
 	{ "Copy VertexB",		"Set the secondary position from current position.",		                PACopyVertexBID			},        
@@ -1439,7 +1439,7 @@ void EPABindVelocityValue::Compile(IWriter& F)
 	pBindVelocityValue(F, _vector("InitialValue").val);
 }
 
-void PS::CPEDef::Render(const Fmatrix& parent)
+/*void PS::CPEDef::Render(const Fmatrix& parent)
 {
 	Fmatrix trans; trans.translate(parent.c);
 	for (EPAVecIt it=m_EActionList.begin(); it!=m_EActionList.end(); it++)
@@ -1447,5 +1447,5 @@ void PS::CPEDef::Render(const Fmatrix& parent)
 			PBool* ar = (*it)->_bool_safe("Allow Rotate");
 			(*it)->Render((ar&&ar->val)?parent:trans);
 		}
-}
+}*/
 
