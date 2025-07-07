f299303e78b9c400ab9afab66bd05d26211b89ac
Shtrecker <gogels235@gmail.com>
2025-07-07 14:07:02 +0300

    Fix switchammotype key flag

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1640,6 +1640,11 @@ bool CWeapon::SwitchAmmoType( u32 flags )
 	if ( !(flags & CMD_START) )
 		return false;
 
+	if (IsMisfire())
+	{
+		return false;
+	}
+
 	if (IsTriStateReload() && iAmmoElapsed == iMagazineSize)
 		return false;
 
