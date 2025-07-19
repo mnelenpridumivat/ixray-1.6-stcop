4414fef3b99c84557b42b71828c48247706aa497
ForserX <sv3nk@yandex.ru>
2025-07-19 22:24:50 +0300

    Cleanup

--- src/xrEngine/EngineThreading.cpp
+++ src/xrEngine/EngineThreading.cpp
@@ -11,7 +11,7 @@
 void XRay::Engine::PreRenderThread()
 {
 	Platform::SetThreadName("X-Ray Pre-Render");
-	PROF_THREAD("Secondary Task 1");
+	PROF_THREAD("X-Ray Pre-Render");
 	{
 		PROF_EVENT("Discord Sync");
 		g_Discord.Update();
@@ -41,7 +41,7 @@ void XRay::Engine::GameThread()
 {
 	Platform::SetThreadName("X-Ray Game Thread");
 
-	PROF_THREAD("Secondary Task 2")
+	PROF_THREAD("X-Ray Game Thread")
 		// we has granted permission to execute
 	{
 		PROF_EVENT("g_hud OnFrameMT")
