95ad85a568c32219293bbbdd862e5fd10d215fd9
ForserX <sv3nk@yandex.ru>
2025-07-19 22:24:51 +0300

    Remove duplicated code in `xrLevelBuilder`

--- src/utils/xrLC_Light/global_calculation_data.cpp
+++ src/utils/xrLC_Light/global_calculation_data.cpp
@@ -152,18 +152,22 @@ void global_claculation_data::xrLoad()
 				if (strchr(N,'.')) *(strchr(N,'.')) = 0;
 				_strlwr			(N);
 
-				if (0==xr_strcmp(N,"level_lods"))	{
+				if (0==xr_strcmp(N,"level_lods"))
+				{
 					// HACK for merged lod textures
 					BT.dwWidth	= 1024;
 					BT.dwHeight	= 1024;
 					BT.bHasAlpha= TRUE;
 					BT.pSurface	= 0;
-					BT.THM.SetHasSurface(FALSE);
-				} else {
-					xr_strcat				(N,sizeof(BT.name),".thm");
-					IReader* THM			= FS.r_open("$game_textures$",N);
+					BT.SetHasSurface(FALSE);
+				}
+				else
+				{
+					xr_strcat(N,sizeof(BT.name),".thm");
+					IReader* THM = FS.r_open("$game_textures$",N);
 
-					if (!THM) {
+					if (!THM)
+					{
 						clMsg("cannot find thm: %s", N);
 						is_thm_missing = true;
 						BT.bHasAlpha = false;
@@ -172,7 +176,7 @@ void global_claculation_data::xrLoad()
 
 						// version
 						u32 version = 0;
-						R_ASSERT(THM->r_chunk(THM_CHUNK_VERSION, &version));
+						R_ASSERT(THM->r_chunk(THM_CHUNK_VERSION,&version));
 						// if( version!=THM_CURRENT_VERSION )	FATAL	("Unsupported version of THM file.");
 
 						// analyze thumbnail information
@@ -188,38 +192,36 @@ void global_claculation_data::xrLoad()
 						BOOL			bLOD = FALSE;
 						if (N[0] == 'l' && N[1] == 'o' && N[2] == 'd' && N[3] == '\\') bLOD = TRUE;
 
-						// load surface if it has an alpha channel or has "implicit lighting" flag
-						BT.dwWidth = BT.THM.width;
-						BT.dwHeight = BT.THM.height;
-						BT.bHasAlpha = BT.THM.HasAlphaChannel();
-						BT.pSurface = 0;
-						BT.THM.SetHasSurface(FALSE);
-						if (!bLOD)
+					// load surface if it has an alpha channel or has "implicit lighting" flag
+					BT.dwWidth				= BT.THM.width;
+					BT.dwHeight				= BT.THM.height;
+					BT.bHasAlpha			= BT.THM.HasAlphaChannel();
+					BT.pSurface				= 0;
+					BT.SetHasSurface(FALSE);
+
+					if (!bLOD) 
+					{
+						if (BT.bHasAlpha || BT.THM.flags.test(STextureParams::flImplicitLighted))
 						{
-							if (BT.bHasAlpha || BT.THM.flags.test(STextureParams::flImplicitLighted))
+							clMsg("- loading: %s",N);
+							u32 w=0, h=0;
+							BT.pSurface = Surface_Load(N,w,h);
+							BT.SetHasSurface(TRUE);
+
+							if (!BT.pSurface)
 							{
-								clMsg("- loading: %s", N);
-								u32			w = 0, h = 0;
-								BT.pSurface = Surface_Load(N, w, h);
-								BT.THM.SetHasSurface(TRUE);
-
-								if (!BT.pSurface) {
-									clMsg("cannot find tga texture: %s", N);
-									is_tga_missing = true;
-									BT.bHasAlpha = false;
-								}
-								else {
-
-									if ((w != BT.dwWidth) || (h != BT.dwHeight)) {
-										Msg("! THM doesn't correspond to the texture: %dx%d -> %dx%d", BT.dwWidth, BT.dwHeight, w, h);
-										BT.dwWidth = w;
-										BT.dwHeight = h;
-									}
-									BT.Vflip();
-								}
+								clMsg("cannot find tga texture: %s", N);
+								is_tga_missing = true;
+								BT.bHasAlpha = false;
 							}
-							else {
-								// Free surface memory
+							else
+							{
+								if ((w != BT.dwWidth) || (h != BT.dwHeight)){
+									Msg("! THM doesn't correspond to the texture: %dx%d -> %dx%d", BT.dwWidth, BT.dwHeight, w, h);
+									BT.dwWidth = w;
+									BT.dwHeight = h;
+								}
+								BT.Vflip	();
 							}
 						}
 					}
