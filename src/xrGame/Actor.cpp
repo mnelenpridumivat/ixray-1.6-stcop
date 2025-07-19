f6dc8a8af44f2d914b95143ea25724a00305906c
Shtrecker <gogels235@gmail.com>
2025-07-19 22:24:50 +0300

    Жду извинений от димы за то что он это нашёл
    А теперь полный фикс чтоб дима не ахуевал
    
    Лан, наебал)))

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1399,13 +1399,18 @@ void CActor::UpdateCL()
 				g_player_hud->load_default();
 			}
 
+			u16 saved_old_slot = NO_ACTIVE_SLOT;
+
 			if (old_slot > 0 && inventory().ItemFromSlot(old_slot) != nullptr)
 			{
+				saved_old_slot = inventory().ItemFromSlot(old_slot)->BaseSlot();
 				inventory().Activate(old_slot);
 				old_slot = 0;
 			}
 
-			if (need_restore_detector && GetDetector(true) != nullptr)
+			bool bres = (saved_old_slot == NO_ACTIVE_SLOT || saved_old_slot == INV_SLOT_2 || saved_old_slot == KNIFE_SLOT || saved_old_slot == BOLT_SLOT);
+
+			if (bres && need_restore_detector && GetDetector(true) != nullptr)
 			{
 				need_restore_detector = false;
 				GetDetector(true)->switch_detector();
