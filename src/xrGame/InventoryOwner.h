4c662873ddbeaf7cb2c192f7a75377208313476e
Shtrecker <gogels235@gmail.com>
2025-07-22 17:39:31 +0300

    Apply virtual cast methods in codebase part 6

--- src/xrGame/InventoryOwner.h
+++ src/xrGame/InventoryOwner.h
@@ -35,6 +35,7 @@ class CAI_Stalker;
 class CEntity;
 class CBaseMonster;
 class CCar;
+class CAI_Trader;
 class CSaveObject;
 
 class CInventoryOwner : public CAttachmentOwner {							
@@ -52,6 +53,7 @@ public:
 	virtual CGameObject*		cast_game_object		()	{return nullptr;}
 	virtual CBaseMonster*		cast_base_monster		()	{return nullptr;}
 	virtual CCar*				cast_car				()	{return nullptr;}
+	virtual CAI_Trader*			cast_trader				()  {return nullptr;}
 public:
 
 	virtual DLL_Pure	*_construct				();
