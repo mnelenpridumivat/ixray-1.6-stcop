406a90916a95828d3feb14bf95f417fa2f5aefa5
mnelenpridumivat <ragrind@mail.ru>
2025-08-09 14:19:20 +0300

    remove print_stack if cannot find object by id

--- src/xrGame/level_script.cpp
+++ src/xrGame/level_script.cpp
@@ -198,7 +198,7 @@ CScriptGameObject* get_object_by_id(u16 id)
 	CGameObject* pGameObject = smart_cast<CGameObject*>(Level().Objects.net_Find(id));
 	if (!pGameObject)
 	{
-		g_pScriptEngine->print_stack();
+		//g_pScriptEngine->print_stack();
 		return nullptr;
 	}
 
