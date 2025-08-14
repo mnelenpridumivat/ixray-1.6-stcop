5b489ded2647ced5882ed00b380a18f640436754
mnelenpridumivat <ragrind@mail.ru>
2025-08-14 19:11:50 +0300

    Implement switching of dynamic wallmarks

--- src/xrGame/DynamicWallmarkZone.h
+++ src/xrGame/DynamicWallmarkZone.h
@@ -4,9 +4,37 @@
 #include "smart_zone.h"
 #include "space_restrictor.h"
 
+class CDynamicWallmarkRegistry
+{	
+	CDynamicWallmarkRegistry();
+
+	xr_hash_map<shared_str, xr_hash_map<shared_str, FactoryPtr<IWallMarkArray>>> registry;
+
+public:
+
+	CDynamicWallmarkRegistry(const CDynamicWallmarkRegistry&) = delete;
+	CDynamicWallmarkRegistry& operator=(const CDynamicWallmarkRegistry&) = delete;
+	CDynamicWallmarkRegistry(CDynamicWallmarkRegistry&&) = delete;
+	CDynamicWallmarkRegistry& operator=(CDynamicWallmarkRegistry&&) = delete;
+	
+	static CDynamicWallmarkRegistry& Instance();
+
+	wm_shader GetWallmarkShader(shared_str shader, shared_str texture);
+};
+
 class CDynamicWallmarkZone : public CSmartZone {
 private:
 	typedef CSmartZone			inherited;
+
+	struct rq_data
+	{
+		Fvector StartPos;
+		Fvector Dir;
+		CDynamicWallmarkZone* self;
+	};
+
+	static BOOL trace_callback(collide::rq_result& result, LPVOID params);
+	static BOOL test_callback(const collide::ray_defs& rd, CObject* object, LPVOID params);
 
 protected:
 
@@ -15,6 +43,7 @@ protected:
 	float h = 1.0f, w = 1.0f, r = 0.0f;
 
 	bool CurrentStatus = false;
+	StaticWallmarkHandle::WallmarkHandlePtr handler = nullptr;
 
 public:
 
