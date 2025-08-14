da60107f1a6bf48c2e6068d8eec7a5ec512039d4
mnelenpridumivat <ragrind@mail.ru>
2025-08-15 00:49:59 +0300

    Reapply "Add Dynamic Wallmark client object"
    This reverts commit a32f2340efba8b621ed648827bab5b99655166e2.

--- src/xrServerEntities/object_factory_register.cpp
+++ src/xrServerEntities/object_factory_register.cpp
@@ -151,6 +151,7 @@
 #include "MissileSam.h"
 #include "Magazine.h"
 #include "SamZone.h"
+#include "DynamicWallmarkZone.h"
 
 #	include "torch.h"
 #	include "pda.h"
@@ -417,7 +418,7 @@ void CObjectFactory::register_classes	()
 	ADD(CNoGravityZone			,CSE_ALifeAnomalousZone			,CLSID_Z_NOGRAVITY				,"nogravity_zone");
 	ADD(CZoneCampfire			,CSE_ALifeAnomalousZone			,CLSID_Z_CAMPFIRE				,"zone_campfire");
 	ADD(CSamZone				,CSE_ALifeSpaceRestrictor		,TEXT2CLSID("SAM_ZONE")			,"sam_zone");
-	ADD(CSpaceRestrictor		,CSE_ALifeDynamicWallmark		,TEXT2CLSID("DYNA_WM")			,"dynamic_wallmark");
+	ADD(CDynamicWallmarkZone	,CSE_ALifeDynamicWallmark		,TEXT2CLSID("DYNA_WM")			,"dynamic_wallmark");
 	// Detectors
 	ADD(CSimpleDetector			,CSE_ALifeItemDetector			,CLSID_DETECTOR_SIMPLE			,"device_detector_simple");
 	ADD(CAdvancedDetector		,CSE_ALifeItemDetector			,CLSID_DETECTOR_ADVANCED		,"device_detector_advanced");
