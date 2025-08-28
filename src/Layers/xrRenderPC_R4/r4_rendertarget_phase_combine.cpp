fe06bd803d54d6806394029030234dd7130983bc
ForserX <sv3nk@yandex.ru>
2025-08-28 21:30:00 +0300

    Add gasmask support
    Partial of 2055
    Thx Lancervrot
    
    Co-Authored-By: Антон Хозаров <59744335+Hozar2002@users.noreply.github.com>

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_combine.cpp
@@ -386,6 +386,12 @@ void CRenderTarget::phase_combine()
 		phase_cas();
 	}
 
+	extern bool UseGasmak;
+	if (UseGasmak)
+	{
+		PhaseGasmask();
+	}
+
 	if (ps_r2_ls_flags_ext.test(R2FLAG_SPP_SATURATION)) {
 		GPU_EVENT(PhaseSaturation);
 		PhaseSaturation();
@@ -400,11 +406,12 @@ void CRenderTarget::phase_combine()
 		GPU_EVENT(PhaseAberration);
 		PhaseAberration();
 	}
+
 	{
 		GPU_EVENT(phase_pp);
 		phase_pp();
 	}
-
+	
 	//	Re-adapt luminance
 	RCache.set_Stencil(FALSE);
 
