f67c6d3fa27393e08921e5a9e9e96dc5c95090da
Hozar2002 <hoz2002team@gmail.com>
2025-09-03 13:37:01 +0300

    Fix `ShaderExternal` cache saving
    Надо потестить, если что допеределаю

--- src/Layers/xrRenderPC_R4/r4.h
+++ src/Layers/xrRenderPC_R4/r4.h
@@ -325,7 +325,7 @@ public:
 
 	xr_string						getShaderParams				();
 	void							addShaderOption				(const char* name, const char* value = "");
-	void							clearAllShaderOptions		() { m_ShaderOptions.clear(); }
+	void							clearAllShaderOptions		();
 
 	auto							ShaderOptionsCount			() { return m_ShaderOptions.size(); }
 
