bef2fa7cc96e5346fa0b1b28d7dcbf934aa8d774
ForserX <sv3nk@yandex.ru>
2025-09-12 15:25:24 +0300

    Remove obsolete files

--- src/xrEngine/GameFont.cpp
+++ src/xrEngine/GameFont.cpp
@@ -463,11 +463,6 @@ float CGameFont::SizeOf_(const char* s)
 	return static_cast<float> (WidthOf(s));
 }
 
-float CGameFont::SizeOf_(const wide_char* wsStr)
-{
-	return 0;
-}
-
 float CGameFont::CurrentHeight_()
 {
 	return GetHeight();
