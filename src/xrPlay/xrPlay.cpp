200585809ab1e3f77b828d023c141aed22612591
St4lker0k765 <126080360+st4lker0k765@users.noreply.github.com>
2025-09-17 22:08:50 +0300

    Show current platform in engine startup info

--- src/xrPlay/xrPlay.cpp
+++ src/xrPlay/xrPlay.cpp
@@ -170,8 +170,8 @@ int APIENTRY WinMain
 
 		Engine.External.Initialize();
 
-		Console->Execute("stat_memory");
-		Msg("IX-Ray CoP %s build info: hash[%s] branch[%s] commit author[%s]", _VER, _HASH, _BRANCH, _AUTHOR);
+	//Console->Execute("stat_memory");
+	Msg("IX-Ray %s %s build info: hash[%s] branch[%s] commit author[%s]", EngineExternal().GetCurrentPlatformFullName(), _VER, _HASH, _BRANCH, _AUTHOR);
 
 		EngineLoadStage4();
 
