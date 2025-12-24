de6824b49182557a4d82c3a576dbbee2db265935
mnelenpridumivat <ragrind@mail.ru>
2025-12-24 16:45:14 +0300

    Fix filewatcher download for non Editor presets -> "Add filewatcher to LocatorAPI to remove rescan_path"

--- cmake/modules.cmake
+++ cmake/modules.cmake
@@ -44,6 +44,4 @@ if(WIN32)
     include("${CMAKE_MODULE_PATHEX}/dirent.cmake")
 endif()
 
-if (IXRAY_EDITORS)
-    include("${CMAKE_MODULE_PATHEX}/FileWatch.cmake")
-endif()
+include("${CMAKE_MODULE_PATHEX}/FileWatch.cmake")
